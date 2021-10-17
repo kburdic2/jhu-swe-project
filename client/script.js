3a8ba6cf09997f2d3f25a2d1d2c0a146e7fe0a71
jdavidlee <90979072+jdavidlee@users.noreply.github.com>
2021-10-17 10:38:18 -0500

    Revert "10172021 base wheel"

--- client/script.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client"
-
-const socket = io('http://localhost:3000')
-
-socket.on('connect', () => {
-    displayMessage(`You connected with id: ${socket.id}`)
-})
-
-socket.emit('custom-event', 10, 'hi', {a: 'a'})
