1d548bf341bd0f5b7bf7e051232b86fea4341548
Nick Roberts <nrober26@jhu.edu>
2021-10-22 19:04:55 -0400

    Added several screens for the UI

--- userinterface/script.js
+++ userinterface/script.js
@@ -1,2 +1,37 @@
 (function() {
+  const connectScreenButton = document.querySelector('.connectButton');
+  const playButton = document.querySelector('.playButton');
+  const spinButton = document.querySelector('.spinButton');
+  const wheel = document.querySelector('.wheel');
+  
+  // Initialize to connect screen
+  document.getElementById("alwaysVisible").style.visibility = 'visible';
+  document.getElementById("connectScreen").style.visibility = 'visible';
+  document.getElementById("alwaysVisibleInGame").style.visibility = 'hidden';
+  document.getElementById("lobbyScreen").style.visibility = 'hidden';
+  document.getElementById("spinScreen").style.visibility = 'hidden';
+
+  // Connect Button Pressed
+  connectScreenButton.addEventListener('click', () => {
+    document.getElementById("connectScreen").style.visibility = 'hidden';
+    document.getElementById("lobbyScreen").style.visibility = 'visible';
+    document.getElementById("alwaysVisibleInGame").style.visibility = 'visible';
   });
+
+  // Play Button Pressed
+  playButton.addEventListener('click', () => {
+    document.getElementById("lobbyScreen").style.visibility = 'hidden';
+    document.getElementById("spinScreen").style.visibility = 'visible';
+    document.getElementById("alwaysVisibleInGame").style.visibility = 'visible';
+  });
+
+  // Spin Button Pressed
+  spinButton.addEventListener('click', () => {
+    document.getElementById("connectScreen").style.visibility = 'visible';
+    document.getElementById("alwaysVisibleInGame").style.visibility = 'visible';
+    document.getElementById("lobbyScreen").style.visibility = 'hidden';
+    document.getElementById("spinScreen").style.visibility = 'hidden';
+    document.getElementById("alwaysVisibleInGame").style.visibility = 'hidden';
+  });
+  
+})();
