11d7f26ef6b1688563a6a91db5418ab1070bd187
Kurt <burdick45k@gmail.com>
2021-10-24 16:48:48 -0400

    Update test suite example

--- unittest/test-math-ex/calculator.test.js
+++ unittest/test-math-ex/calculator.test.js
@@ -1,17 +1,27 @@
 const mathOperations = require('./calculator');
 
-describe("Calculator tests", () => {
+describe("Calculator test1", () => {
  test('adding 1 + 2 should return 3', () => {
    expect(mathOperations.sum(1, 2)).toBe(3);
  });
 })
 
-describe("Calculator tests", () => {
- test('adding 2 + 2 should return 4', () => {
+describe("Calculator test2", () => {
+ test('subtracting 6 - 2 should return 4', () => {
    // arrange and act
-   var result = mathOperations.sum(2,2)
+   var result = mathOperations.diff(6,2)
 
    // assert
    expect(result).toBe(4);
  });
+})
+
+describe("Calculator test3", () => {
+  test('mult 2 * 4 should return 8', () => {
+    // arrange and act
+    var result = mathOperations.product(2,4)
+ 
+    // assert
+    expect(result).toBe(8);
+});
 })
